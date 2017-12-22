1e795ac2cb767c4803030e6e5e686cf909dd1900
Valeriane Aron <valeriane.aron@gmail.com>
2017-12-22 11:59:14 +0100

    steps 1 to 5 ok! Next step = bonus or css finetuning

---
+++ src/AppBundle/Form/GiftType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GiftType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('giftName')->add('giftPictureUrl')->add('giftDescription')->add('giftPrice');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Gift'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_gift';
+    }
+
+
+}
