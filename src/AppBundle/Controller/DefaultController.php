78011c8f4ce3e104a7740ab6e439d20364742786
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2017-12-22 12:22:28 +0100

    Checkpoint #2 * Quizz complété * Swiftmailer fonctionnel mais pas de formulaire de contact * Gift entity fonctionnelle * Category entity fonctionnelle * Pas encore de menu déroulant pour les cat. quand on crée un cadeau

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Gift;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +15,64 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        // On récupère notre entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        // On récupère tous les cadeaux
+        $gifts = $em->getRepository(Gift::class)->findAll();
+        $categories = $em->getRepository(Category::class)->findAll();
+
+        // On affiche la vue en récupérant tous les cadeaux
+        return $this->render('gift/index.html.twig', array(
+            'gifts' => $gifts,
+            'categories' => $categories
+            ));
     }
+
+    /**
+     * @Route("/", name="category")
+     */
+    public function categoryAction()
+    {
+        // On récupère notre entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        // On récupère toutes les catégories
+        $categories = $em->getRepository(Category::class)->findAll();
+
+        // On affiche la vue en récupérant toutes les catégories
+        return $this->render('category/index.html.twig', array(
+            'categories' => $categories
+        ));
+    }
+
+    /**
+     * @Route("/", name="contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('default/contact.html.twig');
+    }
+
+    /**
+     * @Route("/", name="sendmail")
+     */
+/*    public function sendMail($name, \Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Merry Christmas'))
+            ->setFrom('douceurvegetale@mail.fr')
+            ->setTo('florianpdf@gmail.com')
+            ->setBody(
+                $this->renderView(
+                // app/Resources/views/Emails/registration.html.twig
+                    'default/registration.html.twig',
+                    array('name' => $name)
+                ),
+                'text/html'
+            );
+
+        $mailer->send($message);
+
+        return $this->render('default/index.html.twig');
+    }*/
 }
