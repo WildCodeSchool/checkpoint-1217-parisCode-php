949a98114e427777acc51ecf0582b0dc29526856
Cindy <cindy.liwenge@gmail.com>
2017-12-22 13:16:00 +0100

    Remove write actions on Kid entity

--- src/AppBundle/Controller/KidController.php
+++ src/AppBundle/Controller/KidController.php
@@ -31,31 +31,6 @@ class KidController extends Controller
         ));
     }
 
-    /**
-     * Creates a new kid entity.
-     *
-     * @Route("/new", name="kid_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $kid = new Kid();
-        $form = $this->createForm('AppBundle\Form\KidType', $kid);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($kid);
-            $em->flush();
-
-            return $this->redirectToRoute('kid_show', array('id' => $kid->getId()));
-        }
-
-        return $this->render('kid/new.html.twig', array(
-            'kid' => $kid,
-            'form' => $form->createView(),
-        ));
-    }
 
     /**
      * Finds and displays a kid entity.
@@ -72,31 +47,7 @@ class KidController extends Controller
             'delete_form' => $deleteForm->createView(),
         ));
     }
-
-    /**
-     * Displays a form to edit an existing kid entity.
-     *
-     * @Route("/{id}/edit", name="kid_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Kid $kid)
-    {
-        $deleteForm = $this->createDeleteForm($kid);
-        $editForm = $this->createForm('AppBundle\Form\KidType', $kid);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('kid_edit', array('id' => $kid->getId()));
-        }
-
-        return $this->render('kid/edit.html.twig', array(
-            'kid' => $kid,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
+    
 
     /**
      * Deletes a kid entity.
